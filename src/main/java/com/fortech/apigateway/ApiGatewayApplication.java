6d279fd53de894053d4a827e0e9a10b4d7d54ef9
Silviu <silviu.curuciuc@gmail.com>
2018-02-09 16:53:44 +0200

    Enable Zuul

--- src/main/java/com/fortech/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/fortech/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,8 @@ package com.fortech.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -9,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
