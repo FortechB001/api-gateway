87c8f88be4d3d4ebbe68458dd533d93bbd65d933
Marius <aniov_usa@yahoo.com>
2018-02-09 15:47:26 +0200

    ....

--- src/main/java/com/fortech/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/fortech/apigateway/ApiGatewayApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * Zuul Proxy Service - act like a proxy
+ */
+
 @EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
